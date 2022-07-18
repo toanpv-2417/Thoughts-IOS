0e3260e79d279ada6d8f6de69a3cdf2a72c1cce8
Phamtoan <phamtoan2255@gmail.com>
2022-07-19 00:48:40 +0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Throughts-IOS
---
+++ Thoughts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Pham Toan on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
